a79322f145712f3603fa952bca02058895dc73be
ahmadkhan11 <ahmadkhanjadoon11@gmail.com>
2023-05-23 12:25:09 +0500

    Student

---
+++ spring-data-jpa/src/main/java/entity/Student.java
@@ -0,0 +1,17 @@
+package entity;
+
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Student {
+
+  public Long studentId;
+  private String firstName;
+  private String lastName;
+  private String emailId;
+  private String firstName;
+  private String firstName;
+  private String firstName;
+
+}
