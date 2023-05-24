71b58039d46360af52384bd85be6e7eac8eba7a3
ahmadkhan11 <ahmadkhanjadoon11@gmail.com>
2023-05-24 15:20:33 +0500

    application properties added and Student Table

--- spring-data-jpa/src/main/java/entity/Student.java
+++ spring-data-jpa/src/main/java/entity/Student.java
@@ -2,16 +2,26 @@ package entity;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Student {
 
+  @Id
   public Long studentId;
   private String firstName;
   private String lastName;
   private String emailId;
-  private String firstName;
-  private String firstName;
-  private String firstName;
+  private String guardianName;
+  private String guardianEmail;
+  private String guardianMobile;
 
 }
