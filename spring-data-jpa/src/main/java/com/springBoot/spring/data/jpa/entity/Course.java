6ca7e9a258e00fcc7969e25a4558d14f52fcbd29
ahmadkhan11 <ahmadkhanjadoon11@gmail.com>
2023-06-12 14:08:14 +0500

    Many to many relation

--- spring-data-jpa/src/main/java/com/springBoot/spring/data/jpa/entity/Course.java
+++ spring-data-jpa/src/main/java/com/springBoot/spring/data/jpa/entity/Course.java
@@ -6,10 +6,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.SequenceGenerator;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -54,6 +57,22 @@ public class Course {
 
   )
   private Teacher teacher;
+
+  @ManyToMany(
+      cascade = CascadeType.ALL
+  )
+  @JoinTable(
+      name = "student_course_map",
+      joinColumns = @JoinColumn(
+          name = "course_id",
+          referencedColumnName = "courseId  "
+      ),
+      inverseJoinColumns = @JoinColumn(
+
+          name = "student_id",
+          referencedColumnName = "studentId"
+      ))
+  private List<Student> students;
 
 
 }
